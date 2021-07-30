e10b49d9e2969f2be00dccec550eef0c6c08888a
Alex RE <ruiz.euler@gmail.com>
2021-07-30 17:39:26 -0400

    discord ids

--- clip_handler.py
+++ clip_handler.py
@@ -19,10 +19,17 @@ def dt(date_str):
     dt = datetime.strptime(date_str, '%Y-%m-%d')
     return dt
 
-def week_ago(days=7):
+def week_ago(days=14):
     today = datetime.today()
     week_ago = today - timedelta(days)
     return week_ago
+
+def print_discord(uname, discord_dict=None):
+    try:
+        n = discord_dict[uname]
+    except KeyError:
+        n = uname
+    return n
 
 
 # Credentials
@@ -46,5 +53,30 @@ streamers['ping'] = streamers.day > week_ago(days=14)
 
 # Generate posting text TODO
 U = streamers[streamers.ping==True].users.unique()
+
+# User Discord id.
+discord = {
+    'schmooster': '@schmooster#4619',
+    'cksample': '@cksample#6599',
+    'roninblades': '@roninblades#4751',
+    'heckseven': '@heckseven#6413',
+    'Woke-Ass Messiah': '@Woke-Ass Messiah#3187',
+    'D B Pawlan': '@WabiSabi#0256',
+    'm1les': '@m1les#7068',
+    'carnalex': '@carnalex#9125',
+    'robby': '@robby#4024',
+    'Data': '@Data#3968',
+    'chthonicyouth': '@chthonicyouth#9534',
+    'Sh0ckValue': '@Sh0ckValue#5747',
+    'bbartokk': '@bbartokk#1450',
+    'neomono': '@neomono#1213',
+    'rumblesan': '@rumblesan#5137',
+    'ma yir': '@ma yir#9841',
+    'enthusiasticElectrons': '@timcode#3086',
+    'tdk#0114': '@tdk#0114',
+    'T.D.C. DOORS': '@T.D.C. DOORS#9622'
+}
+
+# Get final list
 for u in U:
-    print(u)
+    print(print_discord(u, discord))
